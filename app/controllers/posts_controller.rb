9bf43436412bf178fc8158269500f5c21a4a1fb7
ying-tonkotsu <kurenatsue@gmail.com>
2020-05-28 16:18:49 +0900

    新規投稿ページを作成する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
+  # 一覧ページ
   def index
     @posts = Post.all
   end
+  # 詳細ページ
   def show
     # idはDB内でparamsという変数のハッシュ値となっている
     @id = params[:id]
     # idカラムがparams[:id]dである投稿データを代入
-    @post = Post.find_by(id:params[:id])
+    @post = Post.find_by(id:@id)
+  end
+  # 新規作成ページ
+  def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 # 投稿関係
 # 投稿一覧
   get 'posts/index'
+# 新規投稿作成
+  get 'posts/new' => "posts#new"
+# Createアクション
+  post 'post/create' => "posts#create"
 # 投稿詳細(URLにIDカラムを含める)
+# postsコントローラの他のアクションはこれより上に書く
   get 'posts/:id' => "posts#show"
 end
