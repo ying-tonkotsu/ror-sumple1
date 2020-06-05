d07201fcd07216a337be2f3d6ed5e17089061a79
ying-tonkotsu <kurenatsue@gmail.com>
2020-06-05 18:09:49 +0900

    (pwカラム)バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   # emailカラムのバリデーション（空欄の予防、重複の予防）
   validates :email, {presence: true,uniqueness: true}
+  # passwordカラムのバリデーション（空欄の予防）
+  validates :password, {presence: true}
 end
