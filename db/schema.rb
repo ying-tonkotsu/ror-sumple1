1ac9b19e22234790860e5cbc0f339affb76c0a92
ying-tonkotsu <kurenatsue@gmail.com>
2020-06-05 18:07:59 +0900

    パスワードカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_070838) do
+ActiveRecord::Schema.define(version: 2020_06_05_090508) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_070838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
