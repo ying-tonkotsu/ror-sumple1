b729e16d6522244150780001cb259cf6db09a3d0
ying-tonkotsu <kurenatsue@gmail.com>
2020-06-08 17:51:17 +0900

    ログインしていないユーザへの処理（途中）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
     # 全コントローラーに共通のアクション
     before_action :set_current_user
   # set_current_userメソッドを定義する
+  # ログイン中のユーザー
     def set_current_user
         @current_user = User.find_by(id: session[:user_id])
+    end
+　# ログインしていない場合
+    def authenticate_user
+        if @current_user == nil
+            flash[:notice] = "You need login"
+        end
     end
 end
