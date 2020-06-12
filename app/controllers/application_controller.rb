81a58cedf965481e8a94c49763e15de6dd3caeb7
ying-tonkotsu <kurenatsue@gmail.com>
2020-06-12 17:18:18 +0900

    ログイン中のユーザー情報を@current_user変数にまとめる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+      # before_actionにset_current_userメソッドを指定
+  before_action :set_current_user
+  # set_current_userメソッドを定義
+    def set_current_user
+        @current_user = User.find_by(id: session[:user_id])
+    end
 end
