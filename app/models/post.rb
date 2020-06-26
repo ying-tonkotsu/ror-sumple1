6975d1ac829e617ea3200c79f863da64d533ae1d
ying-tonkotsu <kurenatsue@gmail.com>
2020-06-26 15:11:46 +0900

    インスタンスメゾットを定義する

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
     validates :content, {presence:true,length:{maximum:140}}
     # user_idのバリデーション
     validates :user_id, {presence:true}
+
+# インスタンスメソッドuserを定義
+  def user
+    user = User.find_by(id: self.user_id)
+  end
 end
