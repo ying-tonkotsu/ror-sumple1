587f5ad5c22fffd68bfa10cb5a026bdd8920bd6b
ying-tonkotsu <kurenatsue@gmail.com>
2020-06-26 14:43:44 +0900

    user_idにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
     # バリデーション
     # {presence:true}でそのカラムの値の存在するかどうかチェックする
-    # 最大文字数を140字に設定
+    # contentの最大文字数を140字に設定
     validates :content, {presence:true,length:{maximum:140}}
+    # user_idのバリデーション
+    validates :user_id, {presence:true}
 end
